db628f3e74edce9f4cf1e54ee9b51a39f1c485bc
Rafael S. Calsaverini <rafael.calsaverini@gmail.com>
2012-09-21 15:11:12 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
+import os
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
-long_description = f.read()
-f.close()
+
+def readfile(fname):
+    fh = open(os.path.join(os.path.dirname(__file__), fname))
+    contents = fh.readlines()
+    fh.close()
+    return contents
+
+long_description = '\n'.join(readfile('README.md'))
+requirements = readfile('requirements.txt')
+
 
 setup(
     name='pySolrQuery',
-    version=0.0.1,
+    version='0.0.1',
     description='Python API for Solr queries',
     long_description=long_description,
     url='http://github.com/rcalsaverini/PySolrQuery',
@@ -18,9 +27,8 @@ setup(
     author_email='rafael.calsaverini(at)gmail(dot)com',
     keywords=['Solr', 'search'],
     license='Public Domain',
-    packages = find_packages(),
+    install_requires = requirements,
     classifiers=[
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
         ]
 )
